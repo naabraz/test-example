af0a73dfe3c0201e8253624fb421292aff07b490
Natalia Braz <naabraz@gmail.com>
2018-12-11 16:11:02 -0200

    Adjusting unit tests
    Adjusting unit testing to instantiate a new stack before all tests

--- stack.spec.js
+++ stack.spec.js
@@ -1,14 +1,17 @@
 const Stack = require('./stack')
 
 describe('Stack', () => {
+  let stack
+  beforeEach(() => {
+    stack = new Stack(3)
+  })
+
   it('Should constructs the stack with a given capacity', () => {
-    let stack = new Stack(3)
     expect(stack.items).toEqual([])
     expect(stack.capacity).toBe(3)
   })
 
   it('Should have an isEmpty function that returns true if the stack is empty and false otherwise', () => {
-    let stack = new Stack(3)
     expect(stack.isEmpty()).toBe(true)
     expect(stack.capacity).toBe(3)
     stack.items.push(2)
@@ -16,7 +19,6 @@ describe('Stack', () => {
   })
 
   it('Should have an isFull function that returns true if the stack is full and false otherwise', () => {
-    let stack = new Stack(3)
     expect(stack.isFull()).toBe(false)
     stack.items.push(0, 1, 2)
     expect(stack.isFull()).toBe(true)
@@ -24,7 +26,6 @@ describe('Stack', () => {
 
   describe('Stack push', () => {
     it('Should add a new element on top of the stack', () => {
-      let stack = new Stack(3)
       stack.push(2)
       expect(stack.items[stack.items.length -1]).toBe(2)
     })
